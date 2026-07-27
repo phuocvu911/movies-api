5f25a845cad2374a6bd13d1bebb090880e6a56eb
Hoang Phuoc Vu <iamphuocvux@gmail.com>
2026-07-27 13:51:03 +0300

    create schema for tables

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -6,7 +6,40 @@ import (
 )
 
 const schema = `
+CREATE TABLE IF NOT EXISTS genre (
+    id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE
+);
 
+CREATE TABLE IF NOT EXISTS actor (
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    name       TEXT NOT NULL,
+    birth_date TEXT NOT NULL
+        CHECK (birth_date GLOB '[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]') --for checking ISO8601
+);
+
+CREATE TABLE IF NOT EXISTS movie (
+    id           INTEGER PRIMARY KEY AUTOINCREMENT,
+    title        TEXT NOT NULL,
+    release_year INTEGER,
+    duration     INTEGER  -- minutes
+);
+
+CREATE TABLE IF NOT EXISTS movie_genre (
+    movie_id INTEGER NOT NULL,
+    genre_id INTEGER NOT NULL,
+    PRIMARY KEY (movie_id, genre_id),
+    FOREIGN KEY (movie_id) REFERENCES movie(id) ON DELETE CASCADE,
+    FOREIGN KEY (genre_id) REFERENCES genre(id) ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS movie_actor (
+    movie_id INTEGER NOT NULL,
+    actor_id INTEGER NOT NULL,
+    PRIMARY KEY (movie_id, actor_id),
+    FOREIGN KEY (movie_id) REFERENCES movie(id) ON DELETE CASCADE,
+    FOREIGN KEY (actor_id) REFERENCES actor(id) ON DELETE RESTRICT
+);
 `
 
 func Migrate(db *sql.DB) error {
