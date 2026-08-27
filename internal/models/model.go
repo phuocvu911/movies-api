cff0288e22c2e0944a94dc3df382bd638fb31c9f
Kim Hang Ngo <ngokimhang1@gmail.com>
2026-08-27 14:54:56 +0300

    fix json tag

--- internal/models/model.go
+++ internal/models/model.go
@@ -10,20 +10,20 @@ type Movie struct {
 // MovieRequest is used to decode the request body for creating a movie (POST)
 type MovieRequest struct {
 	Title    string  `json:"title" validate:"required"`
-	Year     int     `json:"release_year" validate:"required,gt=0"`
+	Year     int     `json:"release_year" validate:"required,gt=0,max=2027"`
 	Duration int     `json:"duration" validate:"required,gt=0"`
-	GenreIDs []int64 `json:"genre_ids" validate:"required,dive,gt=0"`
-	ActorIDs []int64 `json:"actor_ids" validate:"required,dive,gt=0"`
+	GenreIDs []int64 `json:"genre_ids" validate:"omitempty,dive,gt=0"`
+	ActorIDs []int64 `json:"actor_ids" validate:"omitempty,dive,gt=0"`
 }
 
 // MoviePatch describes a partial update (PATCH); nil fields are left unchanged
 type MoviePatch struct {
-	ID       int64
+	ID       int64    `json:"id"`
 	Title    *string  `json:"title"`
 	Year     *int     `json:"release_year" validate:"omitempty,gt=0"`
 	Duration *int     `json:"duration" validate:"omitempty,gt=0"`
-	GenreIDs *[]int64 `json:"genre_id" validate:"omitempty,dive,gt=0"`
-	ActorIDs *[]int64 `json:"actor_id" validate:"omitempty,dive,gt=0"`
+	GenreIDs *[]int64 `json:"genre_ids" validate:"omitempty,dive,gt=0"`
+	ActorIDs *[]int64 `json:"actor_ids" validate:"omitempty,dive,gt=0"`
 }
 
 // has all relationships
